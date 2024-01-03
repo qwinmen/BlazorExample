b3578a715cacc192e08202af7d2d80a2c1079d58
Dmitry Safronov <qwinmen@ya.ru>
2024-01-03 22:13:38 +0300

    initialize

---
+++ BlazingTrails.Client/BlazingTrails.Client/Features/Home/Trail.cs
@@ -0,0 +1,27 @@
+namespace BlazingTrails.Client.Features.Home
+{
+	/// <summary>
+	///     Информация о тропе
+	/// </summary>
+	public class Trail
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = "";
+		public string Description { get; set; } = "";
+		public string Image { get; set; } = "";
+		public string Location { get; set; } = "";
+		public int TimeInMinutes { get; set; }
+		public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+		public int Length { get; set; }
+		public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+	}
+
+	/// <summary>
+	///     Путевая точка на маршруте
+	/// </summary>
+	public class RouteInstruction
+	{
+		public int Stage { get; set; }
+		public string Description { get; set; } = "";
+	}
+}
---
+++ BlazingTrails.Client/BlazingTrails.Client/Program.cs
@@ -0,0 +1,14 @@
+using BlazingTrails.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
+//Репозиторий туториала книги "Blazor в действии":
+//https://github.com/chrissainty/blazor-in-action/
