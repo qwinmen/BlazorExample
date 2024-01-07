717bad47b5773a01a0e36d659fffbf19d797c218
Dmitry Safronov <qwinmen@ya.ru>
2024-01-07 20:57:52 +0300

    Подключение проекта API как основного-стартового, включение возможности отладки

--- BlazingTrails/BlazingTrails.Api/Program.cs
+++ BlazingTrails/BlazingTrails.Api/Program.cs
@@ -1,17 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container:
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+	//Позволяет отлаживать код проекта Blazor.Client WebAssembly:
+	app.UseWebAssemblyDebugging();
+}
 app.UseHttpsRedirection();
+//Позволяет API проекту отдавать файлы приложения Blazor.Client:
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 
-app.UseAuthorization();
-
+//app.UseAuthorization();
+app.UseRouting();
 app.MapControllers();
+
+//Если запрос не соответствует контролеру, отдавать файл из Blazor.Client:
+app.MapFallbackToFile("index.html");
 
 app.Run();
