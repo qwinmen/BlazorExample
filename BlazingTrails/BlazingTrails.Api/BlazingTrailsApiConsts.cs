7e23c46a3afcf6a26b21667c308cb5ea1d4cbf43
Dmitry Safronov <qwinmen@ya.ru>
2024-01-28 18:55:43 +0300

    Подключение каталога Images как статического

---
+++ BlazingTrails/BlazingTrails.Api/BlazingTrailsApiConsts.cs
@@ -0,0 +1,10 @@
+namespace BlazingTrails.Api
+{
+	public class BlazingTrailsApiConsts
+	{
+		/// <summary>
+		///     Имя каталога, в котором хранятся изображения, загруженные пользователями. Статический ресурс.
+		/// </summary>
+		public const string ImageStaticDirectory = @"Images";
+	}
+}
--- BlazingTrails/BlazingTrails.Api/Program.cs
+++ BlazingTrails/BlazingTrails.Api/Program.cs
@@ -1,6 +1,8 @@
+using BlazingTrails.Api;
 using BlazingTrails.Api.Persistence;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +30,12 @@ app.UseHttpsRedirection();
 //Позволяет API проекту отдавать файлы приложения Blazor.Client:
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
+//Конфига, которая позволит отдавать статические файлы из указанного каталога:
+app.UseStaticFiles(new StaticFileOptions
+	{
+		FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), BlazingTrailsApiConsts.ImageStaticDirectory)),
+		RequestPath = new PathString(BlazingTrailsApiConsts.ImageStaticDirectory.EnsureStartsWith('/')),
+	});
 
 //app.UseAuthorization();
 app.UseRouting();
